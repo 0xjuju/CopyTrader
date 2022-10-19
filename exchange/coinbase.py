6381017edcdd8e90f068e37756b32c235da56881
Jermol Jupiter <jermol.jupiter@gmail.com>
2022-10-19 01:54:19 -0400

    COinbase API

--- exchange/coinbase.py
+++ exchange/coinbase.py
@@ -1,12 +1,45 @@
+
 import requests
 
-from webstuff.web_functions import request_get_data
+from exchange.models import CoinbaseProduct
+from decouple import config
+from twilio_sms.twilio_api import Twilio
 
 
 class Coinbase:
 
     def __init__(self):
         self.BASE_URL = "https://api.exchange.coinbase.com/"
+        self.json_application_type = "application/json"
+
+    @staticmethod
+    def add_new_products_to_database(products: list[str]) -> None:
+        """
+        Add new tokens to database
+        :param products: list of new tokens on Coinbase
+        :return: None
+        """
+        products = [CoinbaseProduct(base_currency=i) for i in products]
+        CoinbaseProduct.objects.bulk_create(products)
+
+    def check_for_new_products(self) -> list[str]:
+        """
+        Find new product listings by comparing old record with current
+        Send sms alert of new changes
+        Update database with new entries
+        """
+        all_products = [i["base_currency"] for i in self.get_all_products()]
+        old_products = CoinbaseProduct.objects.values_list("base_currency", flat=True)
+        new_products = list(set(old_products).symmetric_difference(set(all_products)))
+
+        # Send sms of new products
+        Twilio().send_sms(body=f"New Products present on Coinbase:\n{new_products}")
+
+        # Update database
+        if new_products:
+            self.add_new_products_to_database(new_products)
+
+        return new_products
 
     def get_all_products(self):
         """
@@ -17,6 +50,11 @@ class Coinbase:
         url = self.BASE_URL + path
         res = requests.get(url, headers=headers)
         return res.json()
+
+    def generate_signature(self, method, path):
+        pass
+
+
 
 
 
--- exchange/models.py
+++ exchange/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class CoinbaseProduct(models.Model):
+    base_currency = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return self.base_currency
