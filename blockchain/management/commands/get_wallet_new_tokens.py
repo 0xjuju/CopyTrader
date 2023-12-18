178b258db5651714772410ad4ae05e54788654cb
Jermol Jupiter <jermol.jupiter@gmail.com>
2023-12-17 19:35:45 -0500

    build function to parse wallet for coingecko tokens

--- blockchain/management/commands/get_wallet_new_tokens.py
+++ blockchain/management/commands/get_wallet_new_tokens.py
@@ -1,5 +1,7 @@
 
 from blockchain.blockchain_explorer import Explorer
+from blockchain.models import Chain, ABI
+from coingecko.models import Address
 from django.core.management.base import BaseCommand
 from wallets.rank_wallets import get_wallets
 
@@ -7,12 +9,27 @@ from wallets.rank_wallets import get_wallets
 class Command(BaseCommand):
     def handle(self, *args, **options):
         wallets = get_wallets()
+        chain_list = Chain.objects.values_list("name", flat=True)
+        abi = ABI.objects.first()
+        gecko_tokens = Address.objects.all()
 
-        for wallet in wallets:
-            held_tokens = wallet.owned_token_set.f
-            for token in held_tokens:
+        for chain in chain_list:
+            explorer = Explorer(chain)
+            gecko_tokens_filtered = gecko_tokens.filter(chain=chain)
+            for wallet in wallets:
+                for token in gecko_tokens_filtered:
+                    address = token.address
 
-                pass
+                    balance = explorer.get_balance_of_token(
+                        wallet_address=wallet,
+                        token_contract_address=address,
+                        abi=abi
+                    )
+
+
+
+
+
 
 
 
