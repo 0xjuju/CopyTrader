031c079f0df6840f87080168ec99d90259bb55cf
Jermol Jupiter <jermol.jupiter@gmail.com>
2023-12-26 15:22:59 -0500

    flatten_list function account for list containing 1 list, and still flattens it

--- algorithms/basic_tools.py
+++ algorithms/basic_tools.py
@@ -2,10 +2,13 @@
 
 def flatten_list(data: list):
     flat_data = list()
-    if len(data) > 1:
-        if isinstance(data[0], list):
+
+    if data and isinstance(data[0], list):
+        if len(data) > 1:
             for row in data:
                 flat_data.extend(row)
+        else:
+            data = data[0]
 
     return flat_data if flat_data else data
 
