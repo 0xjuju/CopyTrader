9572ce1309e85f55d928915091007d18a8594b47
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-16 02:02:53 -0500

    add Gem and Wallet intergration to webhook

--- blockchain/views.py
+++ blockchain/views.py
@@ -4,21 +4,25 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from wallets.rank_wallets import get_wallets
+from wallets.models import Gem, Wallet
 
 
 @csrf_exempt
 def wallet_hook(request):
+
     wallets = get_wallets()
     wallet_address_list = wallets.values_list("address", flat=True)
     wallet_address_list = [i.lower() for i in wallet_address_list] + ["0xbe3f4b43db5eb49d1f48f53443b9abce45da3b79",
-                                                                      "0x7853b3736edba9d7ce681f2a90264307694f97f2"]
+                                                                      ]
 
     body = json.loads(request.body)
 
     events = body["event"]["activity"]
     for event in events:
         if event["toAddress"].lower() in wallet_address_list:
-            print(event["asset"])
-
+            gem, _ = Gem.objects.get_or_create(name=event["name"], address=event["address"])
+            wallet = Wallet.objects.get(address=event["address"])
+            gem.add(wallet)
+            gem.save()
     return HttpResponse(200)
 
