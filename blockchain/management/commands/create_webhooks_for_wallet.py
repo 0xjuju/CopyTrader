d67758c9fb2bed6970b629efd4377d6e3b530222
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-16 17:53:52 -0500

    logic for create webhook command

--- blockchain/management/commands/create_webhooks_for_wallet.py
+++ blockchain/management/commands/create_webhooks_for_wallet.py
@@ -1,14 +1,15 @@
 
 from blockchain.alchemy import Webhook
 from django.core.management.base import BaseCommand
+from wallets.rank_wallets import get_wallets
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         w = Webhook()
-        webhooks = w.get_all_webhooks()
 
-        print(webhooks)
+        active_webhooks = w.get_all_webhooks()
+
 
 
 
