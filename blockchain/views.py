2db9d4501c734c19173b8939471a5c0e46d14a41
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-16 02:19:14 -0500

    fix keyerrors  for model lookups

--- blockchain/views.py
+++ blockchain/views.py
@@ -12,17 +12,17 @@ def wallet_hook(request):
 
     wallets = get_wallets()
     wallet_address_list = wallets.values_list("address", flat=True)
-    wallet_address_list = [i.lower() for i in wallet_address_list] + ["0xbe3f4b43db5eb49d1f48f53443b9abce45da3b79",
-                                                                      ]
+    wallet_address_list = [i.lower() for i in wallet_address_list]
 
     body = json.loads(request.body)
 
     events = body["event"]["activity"]
     for event in events:
         if event["toAddress"].lower() in wallet_address_list:
-            gem, _ = Gem.objects.get_or_create(name=event["name"], address=event["address"])
-            wallet = Wallet.objects.get(address=event["address"])
-            gem.add(wallet)
+            gem, _ = Gem.objects.get_or_create(name=event["asset"], contract_address=event["toAddress"])
+            wallet = Wallet.objects.get(address=event["toAddress"])
+            gem.wallet.add(wallet)
             gem.save()
+
     return HttpResponse(200)
 
