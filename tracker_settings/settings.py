aecf27be0cb891afc86f670a8fdbafbde6026349
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-17 13:06:57 -0500

    SQL database for basic tests, Postgres for production

--- tracker_settings/settings.py
+++ tracker_settings/settings.py
@@ -98,34 +98,34 @@ WSGI_APPLICATION = 'tracker_settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# if 'test' in sys.argv:
-#
-#     DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.sqlite3',
-#                 'NAME': BASE_DIR / 'db.sqlite3',
-#             }
-#         }
-# else:
-#     DATABASES = {
-#         'default': {
-#
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': config("DB_NAME"),
-#             'USER': config("DB_USER"),
-#             'PASSWORD': config("DB_PASSWORD"),
-#             'HOST': config("DB_HOST"),
-#             'PORT': config("DB_PORT"),
-#         }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+
+if 'test' in sys.argv:
+
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config("DB_NAME"),
+            'USER': config("DB_USER"),
+            'PASSWORD': config("DB_PASSWORD"),
+            'HOST': config("DB_HOST"),
+            'PORT': config("DB_PORT"),
+        }
+    }
 
 
 # Password validation
