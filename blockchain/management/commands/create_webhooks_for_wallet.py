316ce4f0ef9b98faa55910a526d80fb92b14b0d1
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-17 00:46:45 -0500

    framwork to add wallets to webhook

--- blockchain/management/commands/create_webhooks_for_wallet.py
+++ blockchain/management/commands/create_webhooks_for_wallet.py
@@ -1,5 +1,6 @@
 
 from blockchain.alchemy import Webhook
+from blockchain.models import AddressWebhook
 from django.core.management.base import BaseCommand
 from wallets.rank_wallets import get_wallets
 
@@ -7,8 +8,13 @@ from wallets.rank_wallets import get_wallets
 class Command(BaseCommand):
     def handle(self, *args, **options):
         w = Webhook()
+        wallets = get_wallets()
+        addresses = wallets.values_list("address")
 
-        active_webhooks = w.get_all_webhooks()
+
+        # w.replace_webhook_address_list()
+
+
 
 
 
