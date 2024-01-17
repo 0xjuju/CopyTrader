c60a74c482b12e3df4fd02efd5bacd83deec14b9
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-17 01:01:46 -0500

    Command to replace entire list of addresses in a webhook with new list

--- blockchain/management/commands/create_webhooks_for_wallet.py
+++ blockchain/management/commands/create_webhooks_for_wallet.py
@@ -9,10 +9,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         w = Webhook()
         wallets = get_wallets()
-        addresses = wallets.values_list("address")
+        addresses = wallets.values_list("address", flat=True)
+        webhook = AddressWebhook.objects.all()
 
-
-        # w.replace_webhook_address_list()
+        # Each instance represents a different chain
+        for each in webhook:
+            chain = each.chain
+            webhook_id = each.webhook_id
+            w.replace_webhook_address_list(address_list=addresses, webhook_id=webhook_id)
 
 
 
