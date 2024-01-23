2458e429eb38df47e677cf2556e547968182e1d3
AJIlentamhen <jermol.jupiter@gmail.com>
2024-01-23 15:16:30 -0500

    setup different function for measuring volatility an percentages in a dataset

--- trading_bot/quant.py
+++ trading_bot/quant.py
@@ -1,18 +1,49 @@
 import numpy as np
 
 
-def calculate_oscillation_score(dataset: list, sampling_rate=1):
+def abs_percent_change(value1, value2, decimal_places=2):
+
+    change = abs(
+        (value2 - value1) / value1
+    ) * 100
+
+    return round(change, decimal_places)
+
+
+def calculate_avg_abs_percentage_change(data):
+    n = len(data)
+
+    # Calculate absolute percentage changes
+    abs_percentage_changes = [abs((data[i] - data[i - 1]) / data[i - 1]) * 100 for i in range(1, n)]
+
+    # Calculate average absolute percentage change
+    avg_abs_percentage_change = sum(abs_percentage_changes) / (n - 1)
+
+    return avg_abs_percentage_change
+
+
+def calculate_oscillation_score(dataset: list):
     differences = np.diff(dataset)
     standard_deviation_of_differences = np.std(differences)
     frequency_score = 1 / (1 + standard_deviation_of_differences)
     return frequency_score
 
 
-def volatility_of_dataset(dataset: list):
-    normalized = normalize_dataset(dataset)
-    standard_deviation = np.std(normalized)
-    coefficient_of_variable = (standard_deviation / np.mean(normalized)) * 100
-    return coefficient_of_variable
+def get_average_percent_change(dataset: list):
+    count = 0
+    pattern = list()
+
+    try:
+        while count < len(dataset):
+            # Get first two values of dataset and calculate its percent change
+            pattern.append(
+                abs_percent_change(dataset[count], dataset[count + 1])
+            )
+            count += 1
+    except IndexError:
+        pass
+
+    return pattern
 
 
 def normalize_dataset(dataset: list):
@@ -21,6 +52,11 @@ def normalize_dataset(dataset: list):
     return [(x - min_value) / (max_value - min_value) for x in dataset]
 
 
+def volatility_of_dataset(dataset: list):
+    normalized = normalize_dataset(dataset)
+    standard_deviation = np.std(normalized)
+    coefficient_of_variable = (standard_deviation / np.mean(normalized)) * 100
+    return coefficient_of_variable
 
 
 
--- trading_bot/tests/test_quant.py
+++ trading_bot/tests/test_quant.py
@@ -8,10 +8,25 @@ class TestQuant(TestCase):
     def setUp(self):
         pass
 
+    def test_calculate_average_percent_change(self):
+        dataset = [0.54, 0.32, 0.12, 0.32, 0.31, 0.30, 0.31, 0.22, 0.19, 0.26]
+        change = calculate_avg_abs_percentage_change(dataset)
+
     def test_calculate_oscillation_of_dataset(self):
         dataset = [1.1, 50, 1.1, 1.1, 1.1]
         frequency = calculate_oscillation_score(dataset)
         self.assertAlmostEquals(frequency, 0.0281, places=3)
+
+        dataset = [0.54, 0.32, 0.12, 0.32, 0.31, 0.30, 0.31, 0.22, 0.19, 0.26]
+        frequency = calculate_oscillation_score(dataset)
+
+    def test_percent_change_from_dataset(self):
+        dataset = [0.25, 0.32, 0.23, 0.22, 0.23, 0.26, 0.33, 0.27, 0.40, 0.32, 0.35]
+        pattern = get_average_percent_change(dataset)
+        self.assertEqual(pattern[0], 28.0)
+        self.assertEqual(pattern[4], 13.04)
+        self.assertEqual(pattern[-1], 9.37)
+
 
 
 
