064fcb5cde624dadd8eef2878e30055c068b1fd8
AJIlentamhen <jermol.jupiter@gmail.com>
2024-01-23 21:17:46 -0500

    add extract_volatile_charts function to trading bot class

--- tracker_settings/trading_bot.py
+++ tracker_settings/trading_bot.py
@@ -14,4 +14,26 @@ class Bot:
         if self.interval not in intervals:
             raise ValueError(f"{self.interval} not an a valid choice. Options are: {intervals}")
 
+    @staticmethod
+    def extract_volatile_charts(datasets: list[list], depth: int, variation_percent: float, vol_threshold: int):
+        if depth > len(datasets):
+            raise IndexError(f"depth {depth} cannot be greater than length of dataset: {len(datasets)}")
+
+        volatile_charts = list()
+        for dataset in datasets:
+            subset = dataset[0:depth]
+            volatility_pattern = get_average_percent_change(subset)
+            frequency_of_vol = 0
+            for value in volatility_pattern:
+                if value >= variation_percent:
+                    frequency_of_vol += 1
+
+            if frequency_of_vol >= vol_threshold:
+                volatile_charts.append(subset)
+
+
+
+
+
+
 
---
+++ trading_bot/tests/test_trading_bot.py
@@ -0,0 +1,16 @@
+
+from django.test import TestCase
+
+
+class TestTradingBot(TestCase):
+    def setUp(self):
+        pass
+
+    def test_extract_volatile_charts(self):
+
+
+
+
+
+
+
