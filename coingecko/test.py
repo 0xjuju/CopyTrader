a32d5939bc82d7276efff6d60171c6379c26811b
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-01-25 01:50:36 -0500

    update coingecko tests

--- coingecko/test.py
+++ coingecko/test.py
@@ -11,22 +11,20 @@ class TestCoingecko(TestCase):
 
     def test_get_coin_data(self):
         data = self.api.get_coin_data("sidus")
-        for d in data:
-            print(d)
+        self.assertEqual(data[0]["name"], "Sidus")
 
     def test_get_asset_platforms(self):
         v = self.api.get_asset_platforms()
 
     def test_get_coin_contract(self):
         data = self.api.get_coin_contract("poof-token")
-        print(data["detail_platforms"])
+        self.assertEqual(data["symbol"], "poof")
 
     def test_get_coins_list(self):
         tokens = self.api.get_coins_list()
 
     def test_get_coins_markets(self):
         res = self.api.get_coins_markets(page=2)
-        print(res[0])
 
     def test_get_market_charts_by_contract(self):
         res = self.api.get_market_chart_by_contract(contract_address=self.nexo_contract_address, days=100,
@@ -41,15 +39,10 @@ class TestCoingecko(TestCase):
 
     def test_parse_collection(self):
         collection = self.api.get_coins_markets(page=4)
-        for token in collection:
-            if token["name"].lower() == "shido":
-                print(token["name"], token["price_change_percentage_24h"], token["price_change_percentage_7d_in_currency"])
 
         self.api.parse_collection(collection=collection, percent_change_24h=40, percent_change_7d=70)
         from .models import GeckoToken
         tokens = GeckoToken.objects.all()
-        print(tokens)
-        # print(tokens.name, tokens.price_change_24hr, tokens.price_change_7d)
 
 
 
