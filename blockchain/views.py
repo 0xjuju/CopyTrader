abf93c2a8f0c502b76943f98e19e70b46cb919f6
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-02-24 02:21:31 -0500

    Update webhoo kview

--- blockchain/views.py
+++ blockchain/views.py
@@ -1,6 +1,8 @@
 import json
 
+from blockchain.alchemy_webhooks import Webhook
 from blockchain.alchemy import Blockchain
+from blockchain.models import ABI
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
@@ -16,10 +18,14 @@ def wallet_hook(request):
     wallet_address_list = [i.lower() for i in wallet_address_list]
 
     body = json.loads(request.body)
-    chain = body["event"]["network"]
+    chain = Webhook().networks[body["event"]["network"]]
+    blockchain = Blockchain(chain)
+    v2pool_abi = ABI.objects.get(abi_type="v2pools").text
+    v3pool_abi = ABI.objects.get(abi_type="v3pools").text
+
     events = body["event"]["activity"]
     for event in events:
-        pass
+        print(event)
         # if event["toAddress"].lower() in wallet_address_list:
         #     gem, _ = Gem.objects.get_or_create(name=event["asset"], contract_address=event["toAddress"])
         #     wallet = Wallet.objects.get(address=event["toAddress"])
