a3bef4bdec954da4a2f383677b1372f071a38b2e
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-02-26 14:35:38 -0500

    Build webhook logic for wallet monitoring

--- blockchain/views.py
+++ blockchain/views.py
@@ -16,22 +16,28 @@ def wallet_hook(request):
     body = json.loads(request.body)
     chain = Webhook().networks[body["event"]["network"]]
     blockchain = Blockchain(chain)
-
+    web_id = body["webhookId"]
+    webhook_wallets = Webhook().get_address_list_from_webhook(web_id)["data"]
     events = body["event"]["activity"]
     for event in events:
-        print(event)
-        logs = event["log"]
-        data = logs["data"]
-        topics = logs["topics"]
-        tx_hash = logs["transactionHash"]
-        decoded_logs = blockchain.get_event(data, topics, "Any")
+        to_address = event["toAddress"]
 
-        gem, _ = Gem.objects.get_or_create(
-            name=event["asset"], contract_address=event["toAddress"], event=decoded_logs.event, transaction_hash=tx_hash
-        )
-        wallet = Wallet.objects.get(address=event["toAddress"])
-        gem.wallet.add(wallet)
-        gem.save()
+        # Wallets that are in the webhook address and received a token represent the user receiving transactions
+        if to_address in webhook_wallets:
+            decoded_logs = blockchain.get_event(data, topics, "Any")
+            if decoded_logs.event not in ["Mint", "Burn", "Withdraw"]:
+
+                logs = event["log"]
+                data = logs["data"]
+                topics = logs["topics"]
+                tx_hash = logs["transactionHash"]
+
+                gem, _ = Gem.objects.get_or_create(
+                    name=event["asset"], event=decoded_logs.event, transaction_hash=tx_hash
+                )
+                wallet = Wallet.objects.get(address=event["toAddress"])
+                gem.wallet.add(wallet)
+                gem.save()
 
     return HttpResponse(200)
 
